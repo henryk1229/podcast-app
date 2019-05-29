e9affcef126e11ea1e2793abbe690af9d286008b
henryk1229 <henryk1229@gmail.com>
2019-05-29 09:21:46 -0400

    format pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
 
   def index
     @users = User.all
@@ -17,10 +19,10 @@ class UsersController < ApplicationController
     if @user.valid?
       @user.save
       log_in @user
-      flash.now[:success] = "Welcome to the Sample App!"
+      flash.now[:message] = "Welcome to the Sample App!"
       redirect_to users_path(@user)
     else
-      flash.now[:notice] = "#{@user.errors.messages.first[0]} "+"#{@user.errors.messages.first[1][0]}"
+      #flash.now[:message] = "#{@user.errors.messages.first[0]} "+"#{@user.errors.messages.first[1][0]}"
       render :new
     end
   end
@@ -31,15 +33,15 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
+    if @user.update_attributes(user_params)
       redirect_to users_path(@user)
     else
-      flash.now[:notice] = "invalid update"
+      #flash.now[:message] = "invalid update"
       render :edit
     end
   end
 
-  def delete
+  def destroy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to login_path
@@ -52,4 +54,18 @@ private
   def user_params(*args)
     params.require(:user).permit(:username, :first_name, :last_name, :email, :password)
   end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to :root unless current_user?(@user)
+  end
+
 end
