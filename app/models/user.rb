3d140198d8cd1488199e051940e9ab0631fc933f
henryk1229 <henryk1229@gmail.com>
2019-05-28 23:49:15 -0400

    implement login w cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   #validations
   before_save { self.email.downcase! }
   validates :first_name,  presence: true, length: { maximum: 50 }
@@ -12,6 +13,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  #attr_accessors
+  attr_accessor :remember_token
+
   #relations
   has_many :favorites
   has_many :podcasts, through: :favorites
@@ -19,6 +23,29 @@ class User < ApplicationRecord
   #methods
 
   def full_name
-    "#{self.first_name} "+"#{self.last_name}" 
+    "#{self.first_name} "+"#{self.last_name}"
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
